d1d337561344588e617c5e0be00fd7511e8d9546
namkai29 <namnvbg29@gmail.com>
2024-05-22 09:33:53 +0700

    NVN776_Cau1

---
+++ Baikiemtra/src/NVN776Person.java
@@ -0,0 +1,12 @@
+public class NVN776Person {
+   
+       private  String hoTen;
+       private int namSinh;
+       private String gioiTinh;
+       public void nhap(){
+        System.out.println("Ho va ten ");
+       }
+
+    
+    
+}
